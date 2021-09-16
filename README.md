4adc9c18cfe9483539e0961fc121b9c45a555f76
Robert Bogan Kang <bob.kang@qq.com>
2021-09-17 02:39:02 +0800

    initial upload (transfer from gitlab)

--- README.md
+++ README.md
@@ -1,1 +1,118 @@
-# midi_translate
+# MIDI  Translate
+
+## Introduction
+
+This project will translate midi file (`.mid`) into `Note`  and `Control` object, and can be used in other purposes.
+
+## Demo
+
+### Translate
+
+Translate midi file into another format (not serialized).
+
+```bash
+python translate.py <path.mid>
+```
+
+### Synthesize
+
+Synthesize midi file into synthesized performance (`piano` or `long note` style).
+
+```bash
+python synthesize.py <path.mid> <out_path.wav>
+```
+
+## Usage
+
+### File `translate.py`
+
+#### Initialize
+
+Initialize class with:
+
+```python
+mt = MidiTranslate(<path.mid>)
+```
+
+#### Translated Result
+
+Notes object extracted from `mt.notes` with format:
+
+```json
+{
+    <channel_0>: [Note_00, Note_01...],
+    <channel_1>: [Note_10...],
+	...
+}
+```
+
+Note parameters:
+
+* `channel`, midi channel;
+* `pitch`, piano key number format;
+* `velocity`, strength of notes to play;
+* `start`, `end`, start and end time of notes;
+* `tick_start`, `tick_end`, start and end tick of notes;
+* `duration`, duration of notes;
+* `ticks`, number of ticks of notes;
+
+Controls object extracted from `mt.controls` with format:
+
+```json
+{
+    <channel_0>: [Control_00, Control_01...],
+    <channel_1>: [Control_10...],
+	...
+}
+```
+
+Control parameters:
+
+* `channel`, midi channel;
+* `control`, control function number;
+* `value`, value of control;
+* `start`, `end`, start and end time of notes;
+* `tick_start`, `tick_end`, start and end tick of notes;
+* `duration`, duration of notes;
+* `ticks`, number of ticks of notes;
+
+#### Apply Sustain Pedal
+
+```python
+mt.apply_sustain_pedal_to_notes()
+```
+
+#### Piano Roll
+
+Plot piano-roll, the piano roll and tempo will be shown.
+
+```python
+mt.pianoroll(<start_time>, <end_time>)
+```
+
+The parameter `start_time` and `end_time` can be none (not given).
+
+### File `synthesize.py`
+
+#### Initialize
+
+Initialize class with:
+
+```python
+s = Synthesize()
+```
+
+The program will synthesize midi files (`.mid` to `.wav`):
+
+```pyt
+synthesize(<path.mid>, <out_path.wav>)
+```
+
+#### Parameters Change
+
+Some parameters could be changed for advanced functions.
+
+* `self.high_velocity_type`, `triangle` or `square` for different sound;
+* `self.piano_sound`, if `False`, it switch to `long note` style;
+* `self.piano_sustain`, if `True`, sustain pedal will be added;
+* `self.reverb_add`, if `True`, reverberate effect will be add;
---
+++ src/translate.py
@@ -0,0 +1,349 @@
+import string
+
+import mido
+import numpy as np
+
+
+class Note(object):
+    """ object of notes """
+
+    def __init__(self, channel=0, pitch=49, velocity=0, start=0, end=0, tick_start=0, tick_end=0):
+        self.channel = channel
+        self.pitch = pitch
+        self.velocity = velocity
+        self.start = start
+        self.end = end
+        self.tick_start = tick_start
+        self.tick_end = tick_end
+        self.duration = 0
+        self.ticks = 0
+
+    def update(self):
+        self.duration = self.end - self.start
+        self.ticks = self.tick_end - self.tick_start
+
+
+class Control(object):
+    """ object of controls """
+
+    def __init__(self, channel=0, control=64, value=0, start=0, end=0, tick_start=0, tick_end=0):
+        self.channel = channel
+        self.control = control
+        self.value = value
+        self.start = start
+        self.end = end
+        self.tick_start = tick_start
+        self.tick_end = tick_end
+        self.duration = 0
+        self.ticks = 0
+
+    def update(self):
+        self.duration = self.end - self.start
+        self.ticks = self.tick_end - self.tick_start
+
+
+class MidiTranslate(object):
+    """
+    midi translate:
+        * convert midi file into `Note` object for audio synthesize
+        * plot piano-roll & tempo
+    get more midi for demo:
+        midi ref: https://github.com/RobertBoganKang/midi_files
+    """
+
+    def __init__(self, mid_path):
+        self.mid_path = mid_path
+        self.mid = None
+        self.tpb = 0
+        self.music_duration = 0
+        self.total_ticks = 0
+        self.note_recipe = {}
+        self.control_recipe = {}
+        self.tempo_recipe = []
+        self.delta_times = []
+        self.tick_idx_to_second = []
+        self.notes = {}
+        self.controls = {}
+        self.pianoroll_min_alpha = 0.2
+        self.piano_key_color = [0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0]
+        self.default_color = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
+                              '#bcbd22', '#17becf', 'b', 'g', 'r', 'c', 'm', 'y', 'k']
+        self.sustain_padel_tolerance_duration = 0.15
+
+        self.initialize()
+        self.translate()
+
+    def initialize(self):
+        self.mid = mido.MidiFile(self.mid_path, clip=True)
+        self.tpb = self.mid.ticks_per_beat
+        self.music_duration = self.mid.length
+
+    def one_tick_to_delta_time(self, tempo_tpu):
+        """
+        convert tick to time in seconds
+        :param tempo_tpu: int; tick per millisecond
+        :return: float; time in seconds per tick
+        """
+        tpu_to_second = tempo_tpu / 1e6
+        beat_per_tick = 1 / self.tpb
+        return beat_per_tick * tpu_to_second
+
+    def delta_time_to_bpm(self, delta):
+        """
+        convert delta time (second per tick) to beat per minute (bpm)
+        :param delta: float; delta time in second
+        :return: float; bpm
+        """
+        return 60 / (delta * self.tpb)
+
+    @staticmethod
+    def get_value_from_msg_by_key(msg, key):
+        return int(msg[msg.rfind(key):].split(' ')[0].split('=')[1].translate(
+            str.maketrans({a: None for a in string.punctuation})))
+
+    def note_on_operation(self, msg, tick):
+        channel = self.get_value_from_msg_by_key(msg, 'channel')
+        note = self.get_value_from_msg_by_key(msg, 'note')
+        velocity = self.get_value_from_msg_by_key(msg, 'velocity')
+        # if channel not exist, get channel
+        if channel not in self.note_recipe:
+            self.note_recipe[channel] = {}
+        # queue implementation
+        if note not in self.note_recipe[channel]:
+            self.note_recipe[channel][note] = [(tick, velocity)]
+        else:
+            self.note_recipe[channel][note].insert(0, (tick, velocity))
+
+    def note_off_operation(self, msg, tick):
+        channel = self.get_value_from_msg_by_key(msg, 'channel')
+        note = self.get_value_from_msg_by_key(msg, 'note')
+        next_tick = tick
+        if note not in self.note_recipe[channel]:
+            raise AttributeError('ERROR: midi format error for notes!')
+        starting_tick, velocity = self.note_recipe[channel][note].pop()
+        ending_tick = next_tick
+        new_note = Note(channel=channel, pitch=note - 20, velocity=velocity, tick_start=starting_tick,
+                        tick_end=ending_tick)
+        if channel not in self.notes:
+            self.notes[channel] = [new_note]
+        else:
+            self.notes[channel].append(new_note)
+
+    def set_tempo_operation(self, msg, tick):
+        tempo = self.get_value_from_msg_by_key(msg, 'tempo')
+        self.tempo_recipe.append((tick, self.one_tick_to_delta_time(tempo)))
+
+    def control_change_operation(self, msg, tick):
+        channel = self.get_value_from_msg_by_key(msg, 'channel')
+        control = self.get_value_from_msg_by_key(msg, 'control')
+        value = self.get_value_from_msg_by_key(msg, 'value')
+        # if channel not exist, get channel
+        if channel not in self.control_recipe:
+            self.control_recipe[channel] = {}
+        # get controls
+        if control not in self.control_recipe[channel]:
+            last_channel = channel
+            last_tick = 0
+            last_value = 0
+        else:
+            last_channel = self.control_recipe[channel][control][0]
+            last_tick = self.control_recipe[channel][control][1]
+            last_value = self.control_recipe[channel][control][2]
+        self.control_recipe[channel][control] = (channel, tick, value)
+        control_obj = Control(channel=channel, control=control, value=last_value, tick_start=last_tick, tick_end=tick)
+        if control_obj.tick_start < control_obj.tick_end and last_channel == channel:
+            if channel not in self.controls:
+                self.controls[channel] = [control_obj]
+            else:
+                self.controls[channel].append(control_obj)
+
+    def add_last_control(self):
+        """ add controls for the last """
+        for ch in self.control_recipe.keys():
+            for control, values in self.control_recipe[ch].items():
+                _, tick_start, value = values
+                control_obj = Control(channel=ch, control=control, value=value, tick_start=tick_start,
+                                      tick_end=self.total_ticks)
+                if control_obj.tick_start < control_obj.tick_end:
+                    if ch not in self.controls:
+                        self.controls[ch] = [control_obj]
+                    else:
+                        self.controls[ch].append(control_obj)
+
+    def create_tempo_tick_to_second(self):
+        tempo_delta_times = []
+        tick = 0
+        for i in range(len(self.tempo_recipe)):
+            if i < len(self.tempo_recipe) - 1:
+                next_tick = self.tempo_recipe[i + 1][0]
+            else:
+                next_tick = self.total_ticks
+            while tick < next_tick:
+                tempo_delta_times.append(self.tempo_recipe[i][1])
+                tick += 1
+        self.delta_times = tempo_delta_times
+        self.tick_idx_to_second = np.add.accumulate([0] + tempo_delta_times)
+
+    def update_time(self, class_obj):
+        for ch in class_obj.keys():
+            for i in range(len(class_obj[ch])):
+                obj = class_obj[ch][i]
+                tick_start = obj.tick_start
+                tick_end = obj.tick_end
+                start = self.tick_idx_to_second[tick_start]
+                end = self.tick_idx_to_second[tick_end]
+                obj.start = start
+                obj.end = end
+                obj.update()
+            class_obj[ch].sort(key=lambda x: x.start)
+
+    def apply_sustain_pedal_to_notes(self):
+        """ apply sustain pedal to the notes """
+        for ch, ch_notes in self.notes.items():
+            ch_control = self.controls[ch]
+            for i in range(len(ch_notes)):
+                note = self.notes[ch][i]
+                note_end = note.end
+                for ctrl in ch_control:
+                    ctrl_start = ctrl.start
+                    ctrl_end = ctrl.end
+                    ctrl_value = ctrl.value
+                    ctrl_control = ctrl.control
+                    if ctrl_control == 64 and ctrl_value > 0 and (
+                            ctrl_start - self.sustain_padel_tolerance_duration < note_end < ctrl_end):
+                        self.notes[ch][i].end = max(note_end, ctrl_end)
+                        self.notes[ch][i].update()
+                        break
+
+    def translate(self):
+        for i in range(len(self.mid.tracks)):
+            track = self.mid.tracks[i]
+            current_tick = 0
+            for msg in track:
+                msg = str(msg)
+                event_time = self.get_value_from_msg_by_key(msg, 'time')
+                next_tick = current_tick + event_time
+                # notes cases
+                if 'note_on' in msg:
+                    self.note_on_operation(msg, next_tick)
+                elif 'note_off' in msg:
+                    self.note_off_operation(msg, next_tick)
+                elif 'set_tempo' in msg:
+                    self.set_tempo_operation(msg, next_tick)
+                elif 'control_change' in msg:
+                    self.control_change_operation(msg, next_tick)
+
+                # update tick
+                current_tick = next_tick
+            self.total_ticks = max(current_tick, self.total_ticks)
+        # add last control
+        self.add_last_control()
+        # build tempo index: tick -> time
+        self.create_tempo_tick_to_second()
+        self.update_time(self.notes)
+        self.update_time(self.controls)
+
+    def plot_pianoroll(self, start_time=None, end_time=None):
+        import matplotlib.pyplot as plt
+
+        # initial translate
+        if not self.notes:
+            self.translate()
+        # 0. fix x-axis range
+        if start_time is None:
+            start_time = 0
+        if end_time is None:
+            end_time = self.music_duration
+
+        # 1. plot notes
+        min_pitch = 1e3
+        max_pitch = -1e3
+        for ch in self.notes.keys():
+            # plot sustain pedal region
+            for key in range(len(self.controls[ch])):
+                obj = self.controls[ch][key]
+                if obj.value != 0 and obj.control == 64:
+                    start = obj.start
+                    end = obj.end
+                    # sustain pedal region
+                    plt.fill([start, end, end, start], [0, 0, 89, 89], facecolor='k', edgecolor='k', alpha=0.1,
+                             zorder=2)
+            # plot notes lines
+            color = self.default_color[ch % len(self.default_color)]
+            for key in range(len(self.notes[ch])):
+                obj = self.notes[ch][key]
+                pitch = obj.pitch
+                velocity = obj.velocity / 127
+                min_pitch = min(pitch, min_pitch)
+                max_pitch = max(pitch, max_pitch)
+                start = obj.start
+                end = obj.end
+                # notes
+                plt.fill([start, end, end, start], [pitch - 0.5, pitch - 0.5, pitch + 0.5, pitch + 0.5],
+                         facecolor=color, edgecolor=color,
+                         alpha=velocity * (1 - self.pianoroll_min_alpha) + self.pianoroll_min_alpha,
+                         zorder=2)
+                # notes velocity line
+                pitch_line_y = pitch - 0.5 + velocity
+                plt.plot([start, end], [pitch_line_y, pitch_line_y], c=color, zorder=3)
+
+        # plot grid-line
+        for key in range(min_pitch - 1, max_pitch + 2):
+            # black key
+            if self.piano_key_color[key % 12 - 4] == 1:
+                plt.fill([0, self.music_duration, self.music_duration, 0], [key - 0.5, key - 0.5, key + 0.5, key + 0.5],
+                         alpha=0.3, facecolor='lightgray', zorder=0)
+            # grid line
+            plt.plot([0, self.music_duration], [key - 0.5, key - 0.5], c='lightgray', linewidth=0.5, zorder=1)
+        # start end edge
+        plt.plot([0, 0], [min_pitch - 1, max_pitch + 1], c='gray', linewidth=2, zorder=3)
+        plt.plot([self.music_duration, self.music_duration], [min_pitch - 1, max_pitch + 1], c='gray', linewidth=3)
+        # plot rest
+        plt.xlabel('time (second)')
+        plt.ylabel('pitch')
+        plt.xlim(start_time, end_time)
+        plt.ylim(min_pitch - 1, max_pitch + 1)
+        plt.show()
+        plt.close()
+
+        # 2. plot tempo
+        ss = [self.delta_time_to_bpm(x) for x in self.delta_times]
+        xx = []
+        for key in range(len(self.delta_times)):
+            xx.append(self.tick_idx_to_second[key])
+        plt.plot(xx, ss, c='k')
+        plt.xlabel('time (second)')
+        plt.ylabel('tempo (bpm)')
+        plt.xlim(start_time, end_time)
+        plt.grid(True)
+        plt.show()
+        plt.close()
+
+
+if __name__ == '__main__':
+    import sys
+
+    mt = MidiTranslate(sys.argv[1])
+    # plot piano-roll
+    mt.plot_pianoroll()
+    # test translate
+    res_notes, res_controls = mt.notes, mt.controls
+    # notes
+    for note_ch, notes in res_notes.items():
+        print(f'--CHANNEL[{note_ch}]--')
+        for n in notes:
+            print(f'PITCH:{str(n.pitch).rjust(3)}\t'
+                  f'VELOCITY:{str(n.velocity).rjust(3)}\t'
+                  f'START:{round(n.start, 3)}\t'
+                  f'END:{round(n.end, 3)}\t'
+                  f'DURATION:{round(n.duration, 3)}')
+    # controls
+    print('-' * 80)
+    for control_ch, controls in res_controls.items():
+        print(f'--CHANNEL[{control_ch}]--')
+        for c in controls:
+            print(f'CONTROL:{str(c.control).rjust(3)}\t'
+                  f'VALUE:{str(c.value).rjust(3)}\t'
+                  f'START:{round(c.start, 3)}\t'
+                  f'END:{round(c.end, 3)}\t'
+                  f'DURATION:{round(c.duration, 3)}')
