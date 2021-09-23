5cd82c2e624ba4f00660a401d5962c0330c030b7
Robert Bogan Kang <bob.kang@qq.com>
2021-09-23 21:07:25 +0800

    more realistic

--- src/sound_library/piano_1.py
+++ src/sound_library/piano_1.py
@@ -21,11 +21,14 @@ class SoundLibrary(SoundCommon):
         self.piano_max_ending_sample_pre_time = 0.05
 
         # piano sound
+        self.piano_sample_max_overtone = 12
+        self.piano_sample_overtone_power_param = 3
         self.piano_final_energy = 0.015
+        self.piano_final_overtone_energy = 0.05
         self.piano_volume_decay_param = 0.7
         self.piano_overtone_decay_param = 0.1
         self.piano_hard_sound_power_param = 0.7
-        self.piano_warping_power_param = 3
+        self.piano_warping_power_param = 2.5
         self.piano_max_f0_volume = 0.39
         self.piano_mix_f0_param_0 = 0.6
         self.piano_mix_f0_param_1 = 0.006
@@ -35,8 +38,8 @@ class SoundLibrary(SoundCommon):
         self.random_unison = 0.7
 
         # system parameters
+        self.sound_library_sin_0 = {}
         self.sound_library_sin_1 = {}
-        self.sound_library_sin_2 = {}
         self.unison_strings = {}
         self.sample_heading = None
 
@@ -61,15 +64,15 @@ class SoundLibrary(SoundCommon):
     def build_sound_library(self):
         for key in range(1, 89):
             random_temperment = random.uniform(-self.random_temperment, self.random_temperment)
+            sin_0_samples = []
             sin_1_samples = []
-            sin_2_samples = []
             for strings in range(self.unison_strings[key]):
                 random_unison = random.uniform(-self.random_unison, self.random_unison)
                 random_shift = random_temperment + random_unison
-                sin_1_samples.append(self.create_sin_sample(key, random_shift, factor=1))
-                sin_2_samples.append(self.create_sin_sample(key, random_shift, factor=2))
+                sin_0_samples.append(self.create_sin_sample(key, random_shift))
+                sin_1_samples.append(self.create_overtone_sample(key, random_shift))
+            self.sound_library_sin_0[key] = np.mean(sin_0_samples, axis=0)
             self.sound_library_sin_1[key] = np.mean(sin_1_samples, axis=0)
-            self.sound_library_sin_2[key] = np.mean(sin_2_samples, axis=0)
 
     def build_heading_caps(self):
         heading_sample = self.sample_heading_time * self.sample_rate
@@ -82,22 +85,39 @@ class SoundLibrary(SoundCommon):
         sample_ending = np.hanning(int(ending_sample * 2))[-int(ending_sample):]
         return sample_ending
 
-    def create_sin_sample(self, key, random_shift, factor=1):
-        frequency = self.key_to_frequency(key, random_shift) * factor
+    def create_sin_sample(self, key, random_shift):
+        f0 = self.key_to_frequency(key, random_shift)
         x = np.arange(0, self.max_note_duration, 1 / self.sample_rate)
-        audio = np.cos(frequency * 2 * np.pi * x)
+        audio = np.cos(f0 * 2 * np.pi * x)
+        return audio
+
+    def create_overtone_sample(self, key, random_shift):
+        f0 = self.key_to_frequency(key, random_shift)
+        x = np.arange(0, self.max_note_duration, 1 / self.sample_rate)
+        audio = np.array([0.0] * len(x))
+        for i in range(2, self.piano_sample_max_overtone + 1):
+            audio += (np.cos(i * f0 * 2 * np.pi * x) / (self.piano_sample_overtone_power_param ** (i - 1)))
+        audio = self.norm_audio(audio)
         return audio
 
     def get_piano_volume_decay(self, key, velocity, length):
         x = np.array([i / self.sample_rate for i in range(length)])
-        volume = (1 - self.piano_final_energy) * (
-                2 + self.piano_volume_decay_param * key * velocity * (key / 88) ** 2) ** (-x) + self.piano_final_energy
-        overtone_volume = (1 + self.piano_overtone_decay_param * key * velocity * (key / 88) ** 2) ** (-x)
+        volume = self.piano_final_energy + (1 - self.piano_final_energy) * (
+                2 + self.piano_volume_decay_param * key * velocity * (key / 88) ** 2) ** (-x)
+        overtone_volume = self.piano_final_overtone_energy + (1 - self.piano_final_overtone_energy) * (
+                1 + self.piano_overtone_decay_param * key * velocity * (key / 88) ** 2) ** (-x)
         return volume, overtone_volume
 
     def get_piano_f0_mix_curve(self, key):
         """ increase low key overtone and reduce high key overtone """
         return self.piano_max_f0_volume * np.power(2, (-self.piano_mix_f0_param_1 * key))
+
+    @staticmethod
+    def apply_power(arr, power):
+        if power % 2 == 1:
+            return np.power(arr, power)
+        else:
+            return np.sign(arr) * np.power(np.abs(arr), power)
 
     def get_sample_single_mic(self, key, velocity, random_sample_shift_idx, sample_length, sample_ending,
                               delta_time, left_mic=True):
@@ -106,9 +126,9 @@ class SoundLibrary(SoundCommon):
         """
         start_idx = self.calculate_start_idx(left_mic, delta_time)
         random_sample_shift_idx += start_idx
-        sin_sample_1 = self.sound_library_sin_1[key][
+        sin_sample_1 = self.sound_library_sin_0[key][
                        random_sample_shift_idx:sample_length + random_sample_shift_idx]
-        sin_sample_2 = self.sound_library_sin_2[key][
+        sin_sample_2 = self.sound_library_sin_1[key][
                        random_sample_shift_idx:sample_length + random_sample_shift_idx]
         # mix sound
         decay_volume, overtone_decay_volume = self.get_piano_volume_decay(key, velocity, sample_length)
@@ -123,7 +143,7 @@ class SoundLibrary(SoundCommon):
         piano_f0_volume = self.get_piano_f0_mix_curve(key)
         final_decay_volume *= piano_f0_volume
         sample = sin_sample_1 * (1 - final_decay_volume) + sin_sample_2 * final_decay_volume
-        sample = self.piano_mix_f0_param_0 * sample + (1 - self.piano_mix_f0_param_0) * np.power(
+        sample = self.piano_mix_f0_param_0 * sample + (1 - self.piano_mix_f0_param_0) * self.apply_power(
             sample, self.piano_warping_power_param)
         # adding caps
         sample[:len(self.sample_heading)] *= self.sample_heading
