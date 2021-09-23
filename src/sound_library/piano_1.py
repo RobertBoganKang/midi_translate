9331f55f1259a228b22015a5a8b91aa5ed850d86
Robert Bogan Kang <bob.kang@qq.com>
2021-09-23 15:55:23 +0800

    better short note

--- src/sound_library/piano_1.py
+++ src/sound_library/piano_1.py
@@ -15,15 +15,15 @@ class SoundLibrary(SoundCommon):
     def __init__(self):
         super().__init__()
         # define heading/ending caps
-        self.sample_heading_time = 0.006
+        self.sample_heading_time = 0.004
         self.piano_max_ending_sample_time = 0.3
         self.piano_max_ending_sample_power_param = 20
-        self.piano_max_ending_sample_pre_time = 0.1
+        self.piano_max_ending_sample_pre_time = 0.05
 
         # piano sound
         self.piano_final_energy = 0.015
         self.piano_volume_decay_param = 0.7
-        self.piano_overtone_decay_param = 0.08
+        self.piano_overtone_decay_param = 0.1
         self.piano_hard_sound_power_param = 0.7
         self.piano_warping_power_param = 3
         self.piano_max_f0_volume = 0.39
